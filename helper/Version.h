eaf36a24548c6859ea653d1f0ebf60bbb5be82a1
Samiro Discher <samiro.discher@rwth-aachen.de>
2023-05-25 18:52:59 +0200

    versioning: remove unused features + add explanation

--- helper/Version.h
+++ helper/Version.h
@@ -75,29 +75,21 @@ static_assert(BUILD_SECOND != UINT32_MAX, "indeterminate build second");
 
 namespace xamid {
 namespace helper {
+// NOTE: More features available at https://github.com/xamidi/pmGenerator/blob/dd29163745c38c3e8d6dd871be6e3929f4dcaa45/helper/Version.h?ts=4#L79-L252
+//       The following approach is rather sophisticated (and a proof of concept) for being able to assign concatenated strings to 'constexpr const char*' variables in C++11.
+//       To only assign concatenated strings to, for example, variables of type 'constexpr std::array<char, [...]>', would be much simpler (example: https://stackoverflow.com/q/28708497),
+//       but std::array<[...]>::data() is not constexpr in C++11.
 
 // Parameter pack helpers (similar to C++14)
-template<typename T, T ... I> struct _integer_sequence {
-	typedef T value_type;
-	static constexpr std::size_t size() noexcept { return sizeof...(I); }
-};
-template<std::size_t ... I> using _index_sequence = _integer_sequence<std::size_t, I...>;
+template<std::size_t ...> struct _index_sequence { using type = _index_sequence; };
 template<std::size_t N, std::size_t ... S> struct gen_pack_sequence: gen_pack_sequence<N - 1, N - 1, S...> {};
-template<std::size_t ... S> struct gen_pack_sequence<0, S...> { typedef _index_sequence<S...> type; };
+template<std::size_t ... S> struct gen_pack_sequence<0, S...> : _index_sequence<S...> {};
 template<std::size_t N> using _make_index_sequence = typename gen_pack_sequence<N>::type;
 
 namespace typelist { // To be able to concatenate constexpr strings despite C++11 restrictions, define a list of types that may contain chars. ; Concept: https://stackoverflow.com/a/275295
 
 template<typename T, T V> struct Value { typedef T type; enum { value = V }; };
 template<char V> struct Char : Value<char, V> {};
-#if 0
-template<bool C, typename A, typename B> struct Conditional { typedef A type; };
-template<typename A, typename B> struct Conditional<false, A, B> { typedef B type; };
-template<typename ...> struct ParameterPack;
-template<bool C, typename = void> struct EnableIf {};
-template<typename Type> struct EnableIf<true, Type> { typedef Type type; };
-template<typename T> struct Identity { typedef T type; };
-#endif
 template<typename ...> struct TypeList;
 template<typename T, typename ... TT> struct TypeList<T, TT...> {
 	typedef T type;
@@ -111,10 +103,6 @@ template<typename ... First, typename ... Second, typename ... Tail> struct Conc
 	typedef typename ConcatList<TypeList<First..., Second...>, Tail...>::type type;
 };
 template<typename T> struct ConcatList<T> { typedef T type; };
-#if 0
-template<typename NewItem, typename List> struct AppendItem;
-template<typename NewItem, typename ...Items> struct AppendItem<NewItem, TypeList<Items...>> { typedef TypeList<Items..., NewItem> type; };
-#endif
 template<typename NewItem, typename List> struct PrependItem;
 template<typename NewItem, typename ... Items> struct PrependItem<NewItem, TypeList<Items...>> { typedef TypeList<NewItem, Items...> type; };
 template<typename List> struct PopFront;
@@ -124,13 +112,6 @@ template<typename List, std::size_t N, typename = void> struct GetItem {
 	typedef typename GetItem<typename List::tail, N - 1>::type type;
 };
 template<typename List> struct GetItem<List, 0> { static_assert(GetSize<List>::value > 0, "index out of bounds"); typedef typename List::type type; };
-#if 0
-template<typename List, template<typename, typename ...> class Matcher, typename... Keys> struct FindItem {
-	static_assert(GetSize<List>::value > 0, "Could not match any item.");
-	typedef typename List::type current_type;
-	typedef typename Conditional<Matcher<current_type, Keys...>::value, Identity<current_type>, FindItem<typename List::tail, Matcher, Keys...>>::type::type type;
-};
-#endif
 template<typename List, std::size_t I, typename NewItem> struct ReplaceItem {
 	static_assert(GetSize<List>::value > 0, "index out of bounds");
 	typedef typename PrependItem<typename List::type, typename ReplaceItem<typename List::tail, I - 1, NewItem>::type>::type type;
@@ -156,15 +137,6 @@ template<char ... Chars> struct char_sequence {
 };
 template<char ... Chars> constexpr char char_sequence<Chars...>::value[];
 template<char ... Chars> struct char_string: char_sequence<Chars..., '\0'> {};
-#if 0
-template<char ... Chars> struct char_array_sequence {
-	static constexpr std::array<char, sizeof...(Chars)> value = { Chars... };
-	typedef typename to_list<Chars...>::type list;
-	template<template<char...> class Template> using param_pack = Template<Chars...>;
-};
-template<char ... Chars> constexpr std::array<char, sizeof...(Chars)> char_array_sequence<Chars...>::value;
-template<char ... Chars> struct char_array_string: char_array_sequence<Chars..., '\0'> {};
-#endif
 template<std::uint8_t ... Digits> struct num_array_string {
 	typedef char_string<('0' + Digits)...> type;
 };
@@ -189,24 +161,6 @@ template<typename CharList, std::size_t ... Indices> struct list_to_string<CharL
 	typedef char_sequence<typelist::GetItem<CharList, Indices>::type::value...> type;
 };
 
-#if 0
-template<typename CharList, std::size_t ... Indices> constexpr std::array<char, sizeof...(Indices)> list_to_array(_index_sequence<Indices...>) {
-	static_assert(sizeof...(Indices) > 0 && typelist::GetItem<CharList, sizeof...(Indices) - 1>::type::value == 0, "missing null-termination");
-	return char_array_sequence<typelist::GetItem<CharList, Indices>::type::value...>::value; // local alternative: { typelist::GetItem<CharList, Indices>::type::value... };
-}
-template<typename CharList> constexpr std::array<char, typelist::GetSize<CharList>::value> list_to_array() {
-	return list_to_array<CharList>(_make_index_sequence<typelist::GetSize<CharList>::value>());
-}
-
-template<typename CharList, std::size_t ... Indices> constexpr const char* list_to_chars(_index_sequence<Indices...>) {
-	static_assert(sizeof...(Indices) > 0 && typelist::GetItem<CharList, sizeof...(Indices) - 1>::type::value == 0, "missing null-termination");
-	return char_sequence<typelist::GetItem<CharList, Indices>::type::value...>::value;
-}
-template<typename CharList> constexpr const char* list_to_chars() {
-	return list_to_chars<CharList>(_make_index_sequence<typelist::GetSize<CharList>::value>());
-}
-#endif
-
 constexpr std::size_t _strlen(char const* s, std::size_t count = 0) {
 	return (*s == '\0') ? count : _strlen(s + 1, count + 1);
 }
@@ -218,20 +172,13 @@ template<typename S> struct struct_to_string { typedef _make_index_sequence<_str
 #define CONSTEXPR_STRING(name, s) \
 	struct name ## __struct { constexpr static const char* get() { return s; } }; \
 	typedef struct_to_string<name ## __struct>::type name
-//template<typename T> struct constexpr_to_list { typedef typename T::template param_pack<to_list>::type type; }; // same as ::list
-
-#if 0
-template<std::size_t X> constexpr std::size_t sum() { return X; }
-template<std::size_t X, std::size_t Y, std::size_t ... Rest> constexpr std::size_t sum() { return X + sum<Y, Rest...>(); }
-// e.g. template<typename T, std::size_t ... Lengths> constexpr std::array<T, sum<Lengths...>() - sizeof...(Lengths) + 1> concatenate(...) { ... }
-#endif
 
 template<typename ... Strings> struct concatenate {
 	template<typename String> using list = typename String::list;
 	typedef typename list_to_string<typename typelist::ConcatStringList<list<Strings>...>::type>::type type;
 };
 
-typedef num_to_string<std::uint32_t, BUILD_YEAR, 4>::type __buildYear; // e.g. char_string<'2', '0', '2', '3'> ; ::value for constexpr char[5] { "2023" } ; ::list for TypeList<Char<'2'>, Char<'0'>, Char<'2'>, Char<'3'>, Char<'\0'>>
+typedef num_to_string<std::uint32_t, BUILD_YEAR, 4>::type __buildYear;
 typedef num_to_string<std::uint32_t, BUILD_MONTH, 2>::type __buildMonth;
 typedef num_to_string<std::uint32_t, BUILD_DAY, 2>::type __buildDay;
 typedef num_to_string<std::uint32_t, BUILD_HOUR, 2>::type __buildHour;
@@ -242,10 +189,10 @@ CONSTEXPR_STRING(__colon, ":");
 CONSTEXPR_STRING(__space, " ");
 CONSTEXPR_STRING(__openBranch, " ('");
 CONSTEXPR_STRING(__closeBranch, "' branch)");
-typedef concatenate<__buildYear, __dot, __buildMonth, __dot, __buildDay>::type __buildDate; // e.g. char_sequence<'2', '0', '2', '3', '.', '0', '5', '.', '2', '4', '\000'>, (equivalent to char_string<'2', '0', '2', '3', '.', '0', '5', '.', '2', '4'>)
+typedef concatenate<__buildYear, __dot, __buildMonth, __dot, __buildDay>::type __buildDate;
 typedef concatenate<__buildHour, __colon, __buildMinute, __colon, __buildSecond>::type __buildTime;
 typedef concatenate<__buildDate, __dot, __buildHour, __buildMinute>::type __buildIdentifier;
-CONSTEXPR_STRING(__name, TOOL_NAME); // e.g. char_string<'p', 'm', 'G', 'e', 'n', 'e', 'r', 'a', 't', 'o', 'r'>
+CONSTEXPR_STRING(__name, TOOL_NAME);
 CONSTEXPR_STRING(__version_spec, TOOL_VERSION_SPEC);
 CONSTEXPR_STRING(__version_branch, TOOL_VERSION_BRANCH);
 typedef concatenate<__name, __space, __version_spec, __space, __buildIdentifier, __openBranch, __version_branch, __closeBranch>::type __version;
